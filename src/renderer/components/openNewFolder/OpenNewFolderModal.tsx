087ccf19aa55ce0cac6355bf83501841d5f78856
Artur-Lepczynski <artur.lepczynski@gmail.com>
2026-02-02 15:58:14 +0100

    fix: better error checking and feedback when selecting folders

--- src/renderer/components/openNewFolder/OpenNewFolderModal.tsx
+++ src/renderer/components/openNewFolder/OpenNewFolderModal.tsx
@@ -1,21 +1,76 @@
-/* eslint-disable no-unused-expressions */
 /* eslint-disable react/jsx-no-bind */
+/* eslint-disable default-case */
+/* eslint-disable consistent-return */
+/* eslint-disable @typescript-eslint/no-shadow */
+/* eslint-disable no-unused-expressions */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import Separator from "../UI/Separator";
 import style from "./OpenNewFolderModal.module.css";
 import GenericButton from "../UI/GenericButton";
 
+type FolderValidationError =
+  | "MISSING_PATH"
+  | "SAME_PATH"
+  | "NESTED_PATH"
+  | "DIFFERENT_FOLDER_NAME";
+
+type folderValidationResult =
+  | { valid: true }
+  | { valid: false; reason: FolderValidationError };
+
 export default function OpenNewFolderModal() {
   const [archiveURL, setArchiveURL] = useState("");
   const [copyURL, setCopyURL] = useState("");
-  const [foldersValid, setFoldersValid] = useState(false);
 
-  useEffect(() => {
-    archiveURL && copyURL && archiveURL !== copyURL
-      ? setFoldersValid(true)
-      : setFoldersValid(false);
+  function validateFolders(
+    archiveURL: string,
+    copyURL: string,
+  ): folderValidationResult {
+    const normalizedArchiveURL = archiveURL.replace(/\\/g, "/");
+    const normalizedCopyURL = copyURL.replace(/\\/g, "/");
+
+    if (!normalizedArchiveURL || !normalizedCopyURL) {
+      return { valid: false, reason: "MISSING_PATH" };
+    }
+
+    if (normalizedArchiveURL === normalizedCopyURL) {
+      return { valid: false, reason: "SAME_PATH" };
+    }
+
+    if (
+      normalizedArchiveURL.startsWith(`${normalizedCopyURL}/`) ||
+      normalizedCopyURL.startsWith(`${normalizedArchiveURL}/`)
+    ) {
+      return { valid: false, reason: "NESTED_PATH" };
+    }
+
+    const archiveLastFolder = normalizedArchiveURL.split("/").pop() || "";
+    const copyLastFolder = normalizedCopyURL.split("/").pop() || "";
+    if (archiveLastFolder !== copyLastFolder) {
+      return { valid: false, reason: "DIFFERENT_FOLDER_NAME" };
+    }
+    return { valid: true };
+  }
+
+  const foldersValid = useMemo(() => {
+    return validateFolders(archiveURL, copyURL);
   }, [archiveURL, copyURL]);
+
+  const errorText = useMemo(() => {
+    if (foldersValid.valid) return "";
+
+    switch (foldersValid.reason) {
+      case "MISSING_PATH":
+        return "";
+      case "SAME_PATH":
+        return "Please choose two different folders";
+      case "NESTED_PATH":
+        return "One of the folders is a subfolder of the other";
+      case "DIFFERENT_FOLDER_NAME":
+        return "Please choose the same logical folder - same folder name, different path";
+    }
+  }, [foldersValid]);
 
   function handleClose() {
     window.electron.closeNewFolderModal();
@@ -70,6 +125,7 @@ export default function OpenNewFolderModal() {
         </div>
       </div>
       <div className={style.controlsWrapper}>
+        <span className={style.errorText}>{!foldersValid || errorText}</span>
         <GenericButton
           className={style.control}
           type="button"
@@ -80,7 +136,7 @@ export default function OpenNewFolderModal() {
         <GenericButton
           className={style.control}
           type="button"
-          disabled={!foldersValid}
+          disabled={!foldersValid.valid}
         >
           Ok
         </GenericButton>
